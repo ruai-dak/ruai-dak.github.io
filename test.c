2710b782b491e6372e4d7e74afc64fdf5e3f4bd8
ruai-dak <98888167+ruai-dak@users.noreply.github.com>
2025-06-27 00:44:27 +0300

    added test c code and second module

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    printf("hello world,my name is ruai\n");
+
+
+ for(int i=0;i<=50;i++){
+    printf("the number on your screen is %d \n",i);
+
+
+
+ }
+ printf("looping interaction done");
+}
